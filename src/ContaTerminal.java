b1b1d71ddb6c6d09ef73a848f4e812330090c5e5
Samuel Marques Silva Filho <samuel.marques@compasso.com.br>
2025-02-01 12:16:52 -0300

    feat: add account class

--- src/ContaTerminal.java
+++ src/ContaTerminal.java
@@ -1,5 +1,52 @@
 public class ContaTerminal {
-    public static void main(String[] args) {
-        System.out.println("Hello Wolrd");
+
+    private Integer numero;
+
+    private String agencia;
+
+    private String titular;
+
+    private Double saldo;
+
+    public ContaTerminal(Integer numero, String agencia, String titular, Double saldo) {
+        this.numero = numero;
+        this.agencia = agencia;
+        this.titular = titular;
+        this.saldo = saldo;
+    }
+
+    public ContaTerminal() {
+    }
+
+    public Integer getNumero() {
+        return numero;
+    }
+
+    public void setNumero(Integer numero) {
+        this.numero = numero;
+    }
+
+    public String getAgencia() {
+        return agencia;
+    }
+
+    public void setAgencia(String agencia) {
+        this.agencia = agencia;
+    }
+
+    public String getTitular() {
+        return titular;
+    }
+
+    public void setTitular(String titular) {
+        this.titular = titular;
+    }
+
+    public Double getSaldo() {
+        return saldo;
+    }
+
+    public void setSaldo(Double saldo) {
+        this.saldo = saldo;
     }
 }
